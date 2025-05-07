1620865e1f971f6d07b519ada1e20ce7de8baa09
RagdeGFC <ragde.gfc@gmail.com>
2025-05-06 18:02:04 -0700

    Commit#7

--- detalles.jsx
+++ detalles.jsx
@@ -23,64 +23,80 @@ function Details() {
 			className="background-container"
 			style={{ backgroundImage: `URL(${pokegif}) ` }}
 		>
-			<Link className="link-back" to="/pokedex">
-				{'🔙'} Volver
-			</Link>
-			<img src={pokemon?.sprites?.other?.showdown?.front_default} alt="" />
+			<div className="pokemon-details-container">
+				<Link className="link-back" to="/pokedex">
+					{'🔙'} Volver
+				</Link>
 
-			<div className="poke__card-header --details">
-				<img src={pokemon?.sprites?.other?.home?.front_default} alt="" />
-				<span className="poke__card-number --details">
-					# {pokemon?.id?.toString().padStart(3, '0')}
-				</span>
-			</div>
+				<div className="pokemon-details-content">
+					<div className="pokemon-image-container">
+						<img
+							className="pokemon-showdown-image"
+							src={pokemon?.sprites?.other?.showdown?.front_default}
+							alt={`${pokemon?.name} showdown sprite`}
+						/>
+						<img
+							className="pokemon-home-image"
+							src={pokemon?.sprites?.other?.home?.front_default}
+							alt={`${pokemon?.name} home sprite`}
+						/>
+						<span className="poke__card-number --details">
+							# {pokemon?.id?.toString().padStart(3, '0')}
+						</span>
+					</div>
 
-			<div className="poke__card-body --details">
-				<h2 className="poke__card-name --details">{pokemon?.name}</h2>
-				<div className="poke__card-label --details">Tipo:</div>
-				<span className="poke__card-types --details">
-					{types?.map((type, index) => {
-						return (
-							<Fragment key={type}>
-								{index > 0 ? (
-									<>
-										/<span key={index + 1}> {tipos[type]} </span>
-									</>
-								) : (
-									<span key={index + 1}> {tipos[type]} </span>
-								)}
-							</Fragment>
-						);
-					})}
-				</span>
-				<p className="poke__card-type-label --details"></p>
-			</div>
-			<div className="poke__card_stats --details">
-				<div className="poke__card-stats-item">
-					<span>HP: </span>
-					<span>{pokemon?.stats[0]?.base_stat}</span>
-				</div>
-				<div className="poke__card-stats-item">
-					<span>Ataque: </span>
-					<span>{pokemon?.stats[1]?.base_stat}</span>
-				</div>
-				<div className="poke__card-stats-item">
-					<span>Defensa: </span>
-					<span>{pokemon?.stats[2]?.base_stat}</span>
-				</div>
-				<div className="poke__card-stats-item">
-					<span>Velocidad: </span>
-					<span>{pokemon?.stats[5]?.base_stat}</span>
-				</div>
-				<div className="poke__card-types --details">
-					<h3>Habilidades: </h3>
-					<div>
-						{pokemon?.abilities?.map((data) => (
-							<span key={data.ability.name}>
-								{' '}
-								<br /> {data.ability.name}
+					<div className="pokemon-info-container">
+						<h2 className="poke__card-name --details">{pokemon?.name}</h2>
+						<div className="pokemon-types">
+							<div className="poke__card-label --details">Tipo:</div>
+							<span className="poke__card-types --details">
+								{types?.map((type, index) => {
+									return (
+										<Fragment key={type}>
+											{index > 0 ? (
+												<>
+													/<span key={index + 1}> {tipos[type]} </span>
+												</>
+											) : (
+												<span key={index + 1}> {tipos[type]} </span>
+											)}
+										</Fragment>
+									);
+								})}
 							</span>
-						))}
+						</div>
+
+						<div className="pokemon-stats-container">
+							<div className="poke__card_stats --details">
+								<div className="poke__card-stats-item">
+									<span>HP: </span>
+									<span>{pokemon?.stats[0]?.base_stat}</span>
+								</div>
+								<div className="poke__card-stats-item">
+									<span>Ataque: </span>
+									<span>{pokemon?.stats[1]?.base_stat}</span>
+								</div>
+								<div className="poke__card-stats-item">
+									<span>Defensa: </span>
+									<span>{pokemon?.stats[2]?.base_stat}</span>
+								</div>
+								<div className="poke__card-stats-item">
+									<span>Velocidad: </span>
+									<span>{pokemon?.stats[5]?.base_stat}</span>
+								</div>
+							</div>
+
+							<div className="pokemon-abilities">
+								<h3>Habilidades: </h3>
+								<div className="abilities-list">
+									{pokemon?.abilities?.map((data) => (
+										<span key={data.ability.name} className="ability-item">
+											{data.ability.name}
+										</span>
+									))}
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
